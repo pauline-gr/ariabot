6c11a35b70b615a7196d04d4e45f8acce1ce0a85
Pauline Grange <pauline.grange.58@gmail.com>
2022-09-04 03:34:32 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,62 @@
+# AriaBot
+
+## Description
+
+Outil discord pour la création de personnages basé sur les règles du jeu [Aria](https://game-of-roles.com/).
+
+Les règles présentées sont disponibles dans les livres du jeu ainsi qu'en version gratuite sur le site d'[Elder Craft](https://elder-craft.com/collections/telechargements?syclid=cc3pvt839i7qmjcil96g)
+
+Autre source: [BaSIC](https://www.sden.org/IMG/pdf/basicregles.pdf)
+
+## Pré-requis
+
+- Node et npm installés avec des versions récentes sur l'hôte'
+- Bot configuré sur https://discord.com/developers/applications
+  - Le bot doit avoir été invité sur un serveur
+  - Le bot doit disposer des droits admin
+  - Disposer du **token** du bot (disponible sur l'espace en ligne > Bot > TOKEN (s'il n'est pas affiché, reset le token))
+
+## Lancement
+
+1. Créer une copie de `config.json.example` à la racine et la renommer `config.json`
+2. Dans `config.json`, modifier `"DISCORD TOKEN https://discord.com/developers/"` par le **token** du bot 
+3. Se positionner dans le dossier et exécuter `npm init` et `npm install`
+4. Lancer l'exécution du bot avec `node index.js`
+
+## Commandes
+
+- Toutes les commandes du bot s'exécutent lorsqu'on le mentionne. 
+- Par défaut son nom est AriaBot. Il est possible de le renommer mais le préfixe de la commande étant la mention, certains copiés-collés pourraient ne pas fonctionner.
+
+## Liste des commandes
+
+### help 
+
+Affiche la liste des commandes disponibles.
+
+### caracInfos 
+
+Affiche les informations sur toutes les caractéristiques
+
+### competencesInfos 
+
+Affiche les informations sur toutes les compétences
+
+### calculComp 
+
+Permet de calculer les compétences en transmettant la liste des caractéristiques dans l'ordre séparées par un espace.
+
+
+Exemple: `calculComp [Force] [Dextérité] [Endurance] [Intelligence] [Charisme]`
+
+Par exemple: `calculComp 12 11 8 6 13`
+
+### compByCaracInfos 
+Affiche la liste simplifiée des compétences et du calcul de caractéristiques lié.
+
+## Sources dev:
+
+- (un peu vieux, à adapter) https://www.digitalocean.com/community/tutorials/how-to-build-a-discord-bot-with-node-js-fr
+- https://discordjs.guide/whats-new.html#pages
+- https://discord.com/developers/docs/topics/gateway#list-of-intents
+- https://discordjs.guide/popular-topics/intents.html#enabling-intents
---
+++ index.js
@@ -0,0 +1,171 @@
+// Récupération des fichiers nécessaires
+const { token } = require("./config.json");
+const { Client, GatewayIntentBits } = require('discord.js');
+const commands = require("./commands.json"); 
+
+const client = new Client({
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+        
+		GatewayIntentBits.MessageContent,
+	],
+});
+
+// Connexion via le Token => Le token doit correspondre au token créé sur https://discord.com/developers
+client.login(token);
+
+// Message log pour vérifier si le bot est bien chargé => c'est le moment où il se connecte à un serveur
+client.on('ready', () => {
+    console.log(`AriaBot est connecté au serveur en tant que ${client.user.tag}!`);
+});
+
+// Sur lecture d'un message
+client.on('messageCreate', msg => {
+  // Si le message a été écrit par le bot lui même on arrête la lecture
+  if (msg.author.bot) return;
+
+  // Si le bot est mentionné dans le message il attend une commande
+  if (msg.mentions.has(client.user.id)) {
+    // Récupération du message complet (comportant l'identifiant user relatif à la mention)
+    let message = msg.content;
+    // Découpage du message pour supprimer la mention
+    message = message.replace(/.*> /, '');
+
+    // Découpage du message en tableau en séparant chaque espace
+    const commandArray = message.split(" ");
+
+    // Si la commande existe dans la liste des commandes json, exécuter le code associé
+    if(commands.hasOwnProperty(commandArray[0])) {
+      switch(commandArray[0]) {
+        // La commande help affiche les informations sur toutes les commandes
+        case "help": 
+          let strHelp = '';
+
+          for (const property in commands) {
+            strHelp += `\n**${property}** \n${commands[property].description}\n`;
+          }
+          msg.reply(`**:keyboard: Liste des commandes :keyboard:** \n${strHelp}`);
+        break;
+
+        // La commande caracInfos retourne la liste des caractéristiques et leurs informations
+        case "caracInfos":
+          let strCaracInfos = '';
+
+          for (const property in commands.caracInfos.infos) {
+            strCaracInfos += `\n**${property}:** \n${commands.caracInfos.infos[property]}\n`;
+          }
+          msg.reply(`**Liste des caractéristiques** \n${strCaracInfos}`);
+        break;
+        
+        // La commande competencesInfos retourne la liste des compétences et leurs informations
+        case "competencesInfos":
+          // Gestion de la liste via un tableau => à cause de la taille max des msg discord de 2000 caractères
+          let arrayCompetencesInfos = [];
+          let strComp = `**Liste des compétences** \n`;
+
+          for (const property in commands.competencesInfos.infos) {
+            let newString = `\n**${property} (${commands.competencesInfos.infos[property].carac1} + ${commands.competencesInfos.infos[property].carac2}):**\n${commands.competencesInfos.infos[property].description}\n`;
+
+            if(strComp.length + newString.length > 2000) {
+              arrayCompetencesInfos.push(strComp);
+              strComp = '';
+            } else {
+              strComp += newString;
+            }
+          }
+          arrayCompetencesInfos.push(strComp);
+
+          arrayCompetencesInfos.forEach(str => msg.reply(`${str}`));
+        
+        break;
+        
+        // La commande calculComp attend en paramètres de commande les caractéristiques dans l'ordre suivant:
+        // Force Dextérité Endurance Intelligence Charisme
+        case "calculComp":
+          console.log(commandArray.length);
+          if(commandArray.length === 6) {
+            let [cmd, forc, dext, end, intel, cha] = commandArray;
+
+            forc = parseInt(forc);
+            dext = parseInt(dext);
+            end = parseInt(end);
+            intel = parseInt(intel);
+            cha = parseInt(cha);
+            
+            if(Number.isInteger(forc), Number.isInteger(dext), Number.isInteger(end), Number.isInteger(intel), Number.isInteger(cha)) {
+              let calculComp = "**Compétences**\n\n";
+              for (const property in commands.competencesInfos.infos) {
+                calculComp += `\n**${property}:** ${getCompByCarac(commandArray, commands.competencesInfos.infos[property].carac1, commands.competencesInfos.infos[property].carac2)}`;
+              }
+              
+              msg.reply(calculComp);
+            } else {
+              msg.reply(`Valeurs fournies NOK:\n
+              Force: ${forc}\n
+              Dextérité: ${dext}\n
+              Endurance: ${end}\n
+              Intelligence: ${intel}\n
+              Charisme: ${cha}\n`);
+            }
+          } else {
+            // Si on a pas les paramètres transmis, on renvoie la description de la commande qui fait office de doc
+            msg.reply(commands.calculComp.description);
+          }
+
+        break;
+
+        // La commande compByCaracInfos retourne une liste simplifiée des compétences et des caractéristiques servant à leur calcul
+        case "compByCaracInfos":
+          let strInfos = '';
+
+          for (const property in commands.competencesInfos.infos) {
+            strInfos += `\n**${property}:** \n${commands.competencesInfos.infos[property].carac1} + ${commands.competencesInfos.infos[property].carac2}\n`;
+          }
+          msg.reply(`**Infos calcul de caractéristiques** \n${strInfos}`);
+          break;
+      }
+    } else {
+      // Decommenter en debug, sinon risque de répondre en cas de tag du bot
+      // msg.reply(`La commande ${commandArray[0]} n'est pas reconnue`);
+      return;
+    }
+  }
+  });
+
+  /**
+   * 
+   * @param {Array} splittedCommand Le tableau du split de la commande
+   * @param {String} carac1 La première carac de calcul
+   * @param {String} carac2 La seconde carac de calcul
+   * @returns le pourcentage de la compétence arrondie à l'entier inférieur
+   */
+  function getCompByCarac(splittedCommand, carac1, carac2) {
+    let c1 = getValueByCarac(splittedCommand, carac1);
+    let c2 = getValueByCarac(splittedCommand, carac2);
+
+    return Math.floor(((c1 + c2) / 2) * 5);
+  }
+
+  /**
+   * 
+   * @param {Array} splittedCommand Le tableau du split de la commande
+   * @param {String} carac La caractéristique à récupérer (Dextérité, Force etc...)
+   * @returns 
+   */
+  function getValueByCarac(splittedCommand, carac) {
+    const [cmd, forc, dext, end, intel, cha] = splittedCommand;
+
+    switch(carac) {
+      case "Force": 
+        return parseInt(forc);
+      case "Dextérité": 
+        return parseInt(dext);
+      case "Endurance": 
+       return parseInt(end);
+      case "Intelligence": 
+       return parseInt(intel);
+      case "Charisme": 
+       return parseInt(cha);
+    }
+  }
